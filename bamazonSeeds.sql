cece81bca405691c591e35de217f54fbd54422aa
Denise Middleton <demiddleton@bellsouth.net>
2019-10-02 18:05:08 -0400

    Updated seed file with database code

--- bamazonSeeds.sql
+++ bamazonSeeds.sql
@@ -8,10 +8,21 @@ CREATE TABLE products (
   id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(45) NOT NULL,
   department_name VARCHAR(25),
-  price INT(100),
-  stock_quantity INT(500),
+  price DECIMAL(10,2) NULL,
+  stock_quantity INT NULL,
   PRIMARY KEY (id)
 );
 
--- INSERT INTO products (flavor, price, quantity)
--- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
+INSERT INTO PRODUCTS (product_name, department_name, price, stock_quantity)
+VALUES
+("Pillow", "Bedding", 5, 50),
+("Comforter", "Bedding", 98.99, 25),
+("Shower Curtain", "Bath", 23.49, 20),
+("Shower Liner", "Bath", 12.99, 35),
+("Blender", "Kitchen", 34.99, 10),
+("Airfryer", "Kitchen", 112.95, 8),
+("Bedskirt", "Bedding", 15, 13),
+("Dishcloth", "Kitchen", 2.99, 46),
+("Rug Set", "Bath", 23.99, 13),
+("Toothbrush Holder", "Bath", 4, 20);
+
